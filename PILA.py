2abae6ab9b1048e00341e563166d252542a3e721
DanielPagesGomez <pagesgomezdaniel@gmail.com>
2024-07-23 14:50:30 +0000

    actualizar3
    actualizar3

--- PILA.py
+++ PILA.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base
 from datetime import datetime
-hola
+hola que tal
 # Configuración base de SQLAlchemy
 Base = declarative_base()
 
