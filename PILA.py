2340bb0e6c8ccdefa4b0de866050e4eda7d1d1de
DanielPagesGomez <pagesgomezdaniel@gmail.com>
2024-07-23 14:57:53 +0000

    eliminar_actualizaciones
    eliminar_actualizaciones

--- PILA.py
+++ PILA.py
@@ -5,8 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base
 from datetime import datetime
-hola que tal dsudfhsuhfisufhsiufhs
-hola
+
 # Configuración base de SQLAlchemy
 Base = declarative_base()
 
